c0f0a93e96db99f156b3343c5472fa2273dae3cf
Jean Centeno <jeancenteno54@gmail.com>
2021-11-04 14:45:26 -0500

    se creo la barra de navegacion

--- src/pages/SearchScreen.jsx
+++ src/pages/SearchScreen.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
 export const SearchScreen = () => {
-  return <div>Hola mundo</div>
+  return (
+    <>
+      <nav className="nav_home">
+        <div>
+          <a href="#">Inicio</a>
+
+          <a href="#">History</a>
+        </div>
+        <div>
+          <a href="#">EN</a>
+          <a href="#">ES</a>
+          <a href="#">PT</a>
+          <button>Cerrar Sesion</button>
+        </div>
+      </nav>
+    </>
+  )
 }
