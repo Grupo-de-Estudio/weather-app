3cf239f15436a66744856b73a53bb81549b1abf1
Jean Centeno <jeancenteno54@gmail.com>
2021-11-05 13:11:12 -0500

    Se ajusto el max-width del input y el contenedor de las tarjetas

---
+++ src/pages/Searchbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Searchbar = () => {
+  return (
+    <div className="div_input">
+      <input type="text" placeholder="Ingrese una ciudad" />
+    </div>
+  )
+}
---
+++ src/pages/SingleCard.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+
+export const SingleCard = () => {
+  return (
+    <div className="cards_content">
+      <div className="cards">
+        <div className="card_image">
+          <img
+            className="clouds"
+            src="https://i.ibb.co/y4GhG0H/cloud.png"
+            alt="cloud"
+            border="0"
+          />
+          <h2>El Tigre</h2>
+          <p>Muy nublado</p>
+          <p>
+            <b>26º</b>
+          </p>
+        </div>
+        <div className="card_down">
+          <p>
+            Temperatura maxima: <b>32º</b>
+          </p>
+          <p>
+            Temperatura minima: <b>20º</b>
+          </p>
+          <p>
+            Sensasion Termica: <b>28º</b>
+          </p>
+          <button className="borrar">Borrar</button>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card_image">
+          <img
+            className="clouds"
+            src="https://i.ibb.co/y4GhG0H/cloud.png"
+            alt="cloud"
+            border="0"
+          />
+          <h2>El Tigre</h2>
+          <p>Muy nublado</p>
+          <p>
+            <b>26º</b>
+          </p>
+        </div>
+        <div className="card_down">
+          <p>
+            Temperatura maxima: <b>32º</b>
+          </p>
+          <p>
+            Temperatura minima: <b>20º</b>
+          </p>
+          <p>
+            Sensasion Termica: <b>28º</b>
+          </p>
+          <button className="borrar">Borrar</button>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card_image">
+          <img
+            className="clouds"
+            src="https://i.ibb.co/y4GhG0H/cloud.png"
+            alt="cloud"
+            border="0"
+          />
+          <h2>El Tigre</h2>
+          <p>Muy nublado</p>
+          <p>
+            <b>26º</b>
+          </p>
+        </div>
+        <div className="card_down">
+          <p>
+            Temperatura maxima: <b>32º</b>
+          </p>
+          <p>
+            Temperatura minima: <b>20º</b>
+          </p>
+          <p>
+            Sensasion Termica: <b>28º</b>
+          </p>
+          <button className="borrar">Borrar</button>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card_image">
+          <img
+            className="clouds"
+            src="https://i.ibb.co/y4GhG0H/cloud.png"
+            alt="cloud"
+            border="0"
+          />
+          <h2>El Tigre</h2>
+          <p>Muy nublado</p>
+          <p>
+            <b>26º</b>
+          </p>
+        </div>
+        <div className="card_down">
+          <p>
+            Temperatura maxima: <b>32º</b>
+          </p>
+          <p>
+            Temperatura minima: <b>20º</b>
+          </p>
+          <p>
+            Sensasion Termica: <b>28º</b>
+          </p>
+          <button className="borrar">Borrar</button>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="card_image">
+          <img
+            className="clouds"
+            src="https://i.ibb.co/y4GhG0H/cloud.png"
+            alt="cloud"
+            border="0"
+          />
+          <h2>El Tigre</h2>
+          <p>Muy nublado</p>
+          <p>
+            <b>26º</b>
+          </p>
+        </div>
+        <div className="card_down">
+          <p>
+            Temperatura maxima: <b>32º</b>
+          </p>
+          <p>
+            Temperatura minima: <b>20º</b>
+          </p>
+          <p>
+            Sensasion Termica: <b>28º</b>
+          </p>
+          <button className="borrar">Borrar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
