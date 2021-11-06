1225abfce339a253a6c17eff88f4863598cd9cdc
Jean Centeno <jeancenteno54@gmail.com>
2021-11-06 08:12:34 -0500

    se cambiaron las etiquetas <b> por <span> y se le asigno la clase 'bold' para ajustar el texto en negrito, se ajusto la barra de busqueda de la pagina SearchScreen

--- src/pages/SearchHistory.jsx
+++ src/pages/SearchHistory.jsx
@@ -4,39 +4,27 @@ export const SearchHistory = () => {
   return (
     <div className="history_content">
       <div className="historys">
-        <p>
-          <b>El Tigre</b>
-        </p>
+        <p className="bold">El Tigre</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
       <div className="historys">
-        <p>
-          <b>Caracas</b>
-        </p>
+        <p className="bold">Caracas</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
       <div className="historys">
-        <p>
-          <b>New York</b>
-        </p>
+        <p className="bold">New York</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
       <div className="historys">
-        <p>
-          <b>Maracay</b>
-        </p>
+        <p className="bold">Maracay</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
       <div className="historys">
-        <p>
-          <b>Acarigua</b>
-        </p>
+        <p className="bold">Acarigua</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
       <div className="historys">
-        <p>
-          <b>Acarigua</b>
-        </p>
+        <p className="bold">Anaco</p>
         <p>Viernes, 05 de Diciembre 2030.</p>
       </div>
     </div>
