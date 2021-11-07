6307692532d926490464916fc1cef9bde0f270c0
Jean Centeno <jeancenteno54@gmail.com>
2021-11-07 03:13:01 -0500

    se hicieron los ajustes finales

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HistoryScreen } from '../pages/HistoryScreen'
-
 import { LoginScreen } from '../pages/LoginScreen'
-
 import { SearchScreen } from '../pages/SearchScreen'
 
 export const AppRouter = () => {
