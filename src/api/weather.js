45f116f9360e8731f695fc6e59fa43ff168621f9
Jean Centeno <jeancenteno54@gmail.com>
2021-11-09 23:33:39 -0500

    se extrajo la informacion del API que se va a usar en las cartas

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,7 +2,23 @@ export const fetchCityWeather = async () => {
   const url =
     'https://api.openweathermap.org/data/2.5/weather?q=El Tigre&lang=es&appid=f639681261a87e68583de41ee2875356&units=metric'
   const resp = await fetch(url)
-  const data = await resp.json()
+  const dataWeather = await resp.json()
 
-  console.log(data)
+  const dataArray = [dataWeather]
+
+  const datos = dataArray.map((dato) => {
+    return {
+      icon: dato.weather[0].icon,
+      ciudad: dato.name,
+      descripcion: dato.weather[0].description,
+      temp: dato.main.temp,
+      max_temp: dato.main.max_temp,
+      min_temp: dato.main.min_temp,
+      sensacion_termica: dato.main.feels_like,
+    }
+  })
+
+  console.log(dataArray)
+
+  console.log(datos)
 }
