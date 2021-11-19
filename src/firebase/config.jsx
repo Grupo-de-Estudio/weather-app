a5bf45bdd3d1c833f587eead1b320efcba744791
Jean Centeno <jeancenteno54@gmail.com>
2021-11-19 18:03:51 -0500

    se instalo y configuro el firebase

---
+++ src/firebase/config.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCoCpg0Qyzhsduy8lFJhxr2W0m_D3Sheck',
+  authDomain: 'proyecto-weather-56f8b.firebaseapp.com',
+  projectId: 'proyecto-weather-56f8b',
+  storageBucket: 'proyecto-weather-56f8b.appspot.com',
+  messagingSenderId: '76962852029',
+  appId: '1:76962852029:web:50be6387f24177d3449bd4',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
